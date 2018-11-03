0a7454b1c6bcda9c7e6c7bc152fc5eaa31ffa4b7
spl.D3V <spedraza.desarrollo@gmail.com>
2018-11-03 10:27:56 +0100

    correcciones ultima hora

--- src/models/Runner.js
+++ src/models/Runner.js
@@ -3,34 +3,25 @@ const {Schema} = mongoose;
 let RunnerSchema = new Schema({
     dorsal: {
         type: Number,
-        unique: true,
-        required: [true, 'campo obligatorio'],
-        min:1,
-        max:4
+        unique: true
     },
     nombre: {
         type: String,
         trim: true,
-        required: [true, 'campo obligatorio'],
         minlength: 3
     },
     apellidos: {
         type: String,
         trim: true,
-        required: [true, 'campo obligatorio'],
         minlength: 3
     },
     categoria: {
         type: Number,
-        required: [true, 'campo obligatorio'],
-        min:1,
-        max:2
+        default: 8
     },
     sexo: {
         type: Number,
-        default: 0,
-        min:1,
-        max:1
+        default: 0
     },
     meta: {
         type: Boolean,
