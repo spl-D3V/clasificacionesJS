175999e838957141430780fa7106317457553aa5
spl.D3V <spedraza.desarrollo@gmail.com>
2019-11-01 16:36:37 +0100

    update categorias

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -5,19 +5,19 @@ function category(anio){
     anio = parseInt(anio);
     if (anio > 2014){
         return 0;
-    }else if(anio > 2012){
+    }else if(anio >= 2012){
         return 1;
-    }else if(anio > 2010){
+    }else if(anio >= 2010){
         return 2;
-    }else if(anio > 2008){
+    }else if(anio >= 2008){
         return 3;
-    }else if(anio > 2006){
+    }else if(anio >= 2006){
         return 4;
-    }else if(anio > 2004){
+    }else if(anio >= 2004){
         return 5;
-    }else if(anio > 2001){
+    }else if(anio >= 2001){
         return 6;
-    }else if(anio > 1982){
+    }else if(anio >= 1982){
         return 7;
     }else if(anio < 1983){
         return 8;
